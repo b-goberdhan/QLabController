4a77f39048273b0fb331a442f6fe25b9a047dc7e
Brandon G <brandongob@gmail.com>
2019-03-20 20:03:45 -0600

    some kinda change

--- Examples/BoxPropDevice/BoxPropDeviceTCP/BoxPropDeviceTCP.ino
+++ Examples/BoxPropDevice/BoxPropDeviceTCP/BoxPropDeviceTCP.ino
@@ -1,77 +1,180 @@
-#include <ArduinoJson.hpp>
-#include <ArduinoJson.h>
+/*
+  WiFi Web Server LED Blink
+
+  A simple web server that lets you blink an LED via the web.
+  This sketch will create a new access point (with no password).
+  It will then launch a new server and print out the IP address
+  to the Serial monitor. From there, you can open that address in a web browser
+  to turn on and off the LED on pin 13.
+
+  If the IP address of your shield is yourAddress:
+    http://yourAddress/H turns the LED on
+    http://yourAddress/L turns it off
+
+  created 25 Nov 2012
+  by Tom Igoe
+  adapted to WiFi AP by Adafruit
+ */
+
 #include <SPI.h>
 #include <WiFi101.h>
-const int capacity = JSON_OBJECT_SIZE(10);
-StaticJsonBuffer<capacity> jb;
+//#include "arduino_secrets.h" 
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "arduinoProp";        // your network SSID (name)
+char pass[] = "props";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                // your network key Index number (needed only for WEP)
 
-const int pingPin = 12;
-const int lightPin = A0;
-
-
-char ssid[] = "EngSystems";          //  your network SSID (name) 
-char pass[] = "engsystems3441";   // your network password
-
+int led =  LED_BUILTIN;
 int status = WL_IDLE_STATUS;
-IPAddress server(74,125,115,105);  // Google
-
-// Initialize the client library
-WiFiClient client;
+WiFiServer server(80);
 
 void setup() {
+  //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; //wait for serial port to connect!
+    ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.println("Attempting to connect to WPA network...");
-  Serial.print("SSID: ");
+
+  Serial.println("Access Point Web Server");
+
+  pinMode(led, OUTPUT);      // set the LED pin mode
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // by default the local IP address of will be 192.168.1.1
+  // you can override it with the following:
+  // WiFi.config(IPAddress(10, 0, 0, 1));
+
+  // print the network name (SSID);
+  Serial.print("Creating access point named: ");
   Serial.println(ssid);
 
-  status = WiFi.begin(ssid, pass);
-  if ( status != WL_CONNECTED) { 
-    Serial.println("Couldn't get a wifi connection");
-    // don't do anything else:
-    while(true);
-  } 
-  else {
-    Serial.println("Connected to wifi");
-    Serial.println("\nStarting connection...");
-    // if you get a connection, report back via serial:
-    if (client.connect(server, 80)) {
-      Serial.println("connected");
-      // Make a HTTP request:
-      client.println("GET /search?q=arduino HTTP/1.0");
-      client.println();
+  // Create open network. Change this line if you want to create an WEP network:
+  status = WiFi.beginAP(ssid);
+  if (status != WL_AP_LISTENING) {
+    Serial.println("Creating access point failed");
+    // don't continue
+    while (true);
+  }
+
+  // wait 10 seconds for connection:
+  delay(10000);
+
+  // start the web server on port 80
+  server.begin();
+
+  // you're connected now, so print out the status
+  printWiFiStatus();
+}
+
+
+void loop() {
+  // compare the previous status to the current status
+  if (status != WiFi.status()) {
+    // it has changed update the variable
+    status = WiFi.status();
+
+    if (status == WL_AP_CONNECTED) {
+      byte remoteMac[6];
+
+      // a device has connected to the AP
+      Serial.print("Device connected to AP, MAC address: ");
+      WiFi.APClientMacAddress(remoteMac);
+      printMacAddress(remoteMac);
+    } else {
+      // a device has disconnected from the AP, and we are back in listening mode
+      Serial.println("Device disconnected from AP");
     }
+  }
+  
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+     
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(led, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(led, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
   }
 }
 
-void loop() {
-  //char output[1064];
-  //delay(100);
-  //long distanceCm = getPingRangeCm();
-  //long lightIntensity = analogRead(lightPin);
-  //buildSensorData(output, sizeof(output), "Light", lightIntensity);
-  //Serial.println(output);
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+
 }
 
-void buildSensorData(char *dest, int destSize, char sensorName[], long value) {
-  JsonObject& data = jb.createObject();
-  data["Name"] = sensorName;
-  data["SensorValue"] = value;
-  data.printTo(dest, destSize);
-  jb.clear();
-}
-
-long getPingRangeCm() {
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
-
-  pinMode(pingPin, INPUT);
-  long duration = pulseIn(pingPin, HIGH);
-  return duration / 29 / 2;
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
 }
